ec5688c03774f8629fcddb235c4748f28c56d9be
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-05-30 01:28:00 +0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,12 @@
 import * as fs from "fs";
+import zlib from "zlib";
 
 const args = process.argv.slice(2);
 const command = args[0];
 
 enum Commands {
   Init = "init",
+  CatFile = "cat-file",
 }
 
 switch (command) {
@@ -14,6 +16,16 @@ switch (command) {
     fs.mkdirSync(".git/refs", { recursive: true });
     fs.writeFileSync(".git/HEAD", "ref: refs/heads/main\n");
     console.log("Initialized git directory");
+    break;
+  case Commands.CatFile:
+    const hash = args[2];
+    const dir = hash.substring(0, 2);
+    const file = hash.substring(2);
+    const blob = fs.readFileSync(`.git/objects/${dir}/${file}`);
+    const buff = zlib.unzipSync(blob);
+    const data = buff.toString();
+    const [_, content] = data.split("\0");
+    process.stdout.write(content);
     break;
   default:
     throw new Error(`Unknown command ${command}`);
